3da7cfb2ca14bec11b9bfa8d83d1e959c3af8360
husnahadi <husnahadi123@gmail.com>
2015-12-02 22:48:39 -0800

    added queriesini.php as separate file

---
+++ queriesini.php
@@ -0,0 +1,12 @@
+<?php
+
+$inifile = "queries.ini"; 
+if (!$queries = parse_ini_file($inifile, TRUE)) throw new 
+exception('Unable to open ' . $file . '.'); 
+
+$user_query = $queries['queries']['users'];  
+$proj_query = $queries['queries']['projs'];  
+$org_query = = $queries['queries']['orgs']; 
+
+
+?>
