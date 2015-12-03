d9cf56760220cc0c8ab162da1972e681dd782b11
Erik Krogen <erikkrogen@gmail.com>
2015-12-03 02:19:08 -0800

    DB results now display on the welcome.php page

--- welcome.php
+++ welcome.php
@@ -5,10 +5,11 @@
 <body>
 
 <?php
-	include 'queriesini.php';
-	include 'mysqlini.php';
-	$db = new mysqli($host, $user, $pw, $dbname);
-	$queries = array("Users"=>$user_query, "Proj"=>$proj_query, "Orgs"=>$org_query);
+   include 'queriesini.php';
+   include 'mysqlini.php';
+   $queries = array("Users"=>$user_query, "Proj"=>$proj_query, "Orgs"=>$org_query);
+   mysql_connect($host, $user, $pw);
+   mysql_select_db($dbname);
 ?>
 
 Your selection is: <?php echo $_POST["top10"]; ?>
@@ -16,35 +17,23 @@ Your selection is: <?php echo $_POST["top10"]; ?>
 <p>
 Query to run is:
 <?php 
-	$query_result = $queries[$_POST["top10"]];
-	echo $query_result 
+   $query_result = $queries[$_POST["top10"]];
+   echo $query_result 
 ?>
+</p>
 
 <p>
 <?php
-	if($db->connect_errorno){
-		echo "<p>";
-		echo "Connection failed!";
-	}else{
-		echo "<p>";
-		echo "Connected!";
-		if($result = $db->query($query_result)){ 
-			echo $result->num_rows; 
-			while($row = $result->fetch-array()){ 
-				echo $row[‘Name’]; 
-			} 
-			$result->free();
-		}
-	}
-
-
-
-
-
-
+   $result = mysql_query($query_result);
+   echo "Number of Rows returned: ".mysql_num_rows($result);
+   while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+     echo "<br/>";
+     foreach($row as $col) {
+       echo $col." | ";
+     }
+   }
 ?>
-
-
+</p>
 
 </body>
 </html>
