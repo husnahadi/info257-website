4086a4a2f3126512d09ae7e1efccdd2050b417f9
Erik Krogen <erikkrogen@gmail.com>
2015-12-03 18:36:07 -0800

    Added languages to the project info page

--- project.php
+++ project.php
@@ -39,12 +39,26 @@
                        $owner_name = $owner_row['github_username'];
                      }
 
+                     $lang_result = mysql_query("SELECT language_occurrence.bytes AS bytes, language.name AS name FROM language_occurrence JOIN language USING (lang_id) WHERE language_occurrence.proj_id = ".$proj['proj_id']." ORDER BY bytes DESC;");
+                     $lang_name_array = array();
+                     $lang_byte_array = array();
+                     while ($lang_row = mysql_fetch_array($lang_result, MYSQL_ASSOC)) {
+                       array_push($lang_name_array, $lang_row['name']);
+                       array_push($lang_byte_array, $lang_row['bytes']);
+                     }
+
                      echo '<p>Project: <a href="http://github.com/'.$owner_name.'/'.$proj['name'].'/">'.$proj['name']."</a>";
                      echo "<p>Date project was started: ".$proj['start_date'];
                      echo "<p>Number of people watching this project: ".$proj['watchers_count'];
                      echo "<p>Number of times this project has been forked: ".$proj['fork_count'];
                      echo '<p>Type of license used: <a href="http://choosealicense.com/licenses/'.$lic_row['short_name'].'">'.$lic_row['full_name']."</a>";
                      echo '<p>Owner of the project: <a href="http://github.com/'.$owner_name.'/">'.$owner_name."</a>";
+                     echo '<p>Languages used in this project:';
+                     echo '<table><tr><th>Name</th><th>Bytes of Content</th></tr>';
+                     for ($i = 0; $i < count($lang_name_array); $i++) {
+                       echo '<tr><td>'.$lang_name_array[$i].'</td><td>'.$lang_byte_array[$i].'</td></tr>';
+                     }
+                     echo '</table>';
                    } else {
                      echo "Could not find any project with name: ".$_POST["project_name"];
                    }
